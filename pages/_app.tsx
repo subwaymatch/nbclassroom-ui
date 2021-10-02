7518f40bd09a4019c5069f09e123dd3c2b1e40ec
Ye Joo Park <subwaymatch@gmail.com>
2021-10-02 01:17:07 -0500

    Setup with Typescript and Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "styles/globals.scss";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const theme = extendTheme({
+  styles: {
+    global: (props: any) => ({
+      "html, body": {
+        fontFamily: "Inter, sans-serif",
+        letterSpacing: "-0.01rem",
+      },
+    }),
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-export default MyApp
+
+export default MyApp;
---
+++ pages/test.tsx
@@ -0,0 +1,311 @@
+import SidebarWithHeader from "components/SidebarWithHeader";
+
+export default function Upload() {
+  return (
+    <SidebarWithHeader>
+      <h2>The best thing you can do</h2>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+        odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+        quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
+        mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+        Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad
+        litora torquent per conubia nostra, per inceptos himenaeos. Curabitur
+        sodales ligula in libero.{" "}
+      </p>
+
+      <p>
+        Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean
+        quam. In scelerisque sem at dolor. <b>Mauris massa</b>. Maecenas mattis.
+        Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor.{" "}
+        <b>Curabitur tortor</b>. Morbi lectus risus, iaculis vel, suscipit quis,
+        luctus non, massa.{" "}
+        <b>
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+          per inceptos himenaeos
+        </b>
+        . Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus
+        metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque
+        volutpat condimentum velit.{" "}
+      </p>
+
+      <p>
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+        inceptos himenaeos. Nam nec ante. <i>Curabitur tortor</i>. Sed lacinia,
+        urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum
+        ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti.
+        Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin
+        quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed
+        lectus.{" "}
+      </p>
+
+      <p>
+        Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra
+        auctor, sem massa mattis sem, at interdum magna augue eget diam.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed
+        non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit
+        amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel,
+        egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices
+        ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar
+        ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa.{" "}
+      </p>
+
+      <p>
+        Cras metus. Sed aliquet risus a tortor.{" "}
+        <i>Suspendisse in justo eu magna luctus suscipit</i>. Integer id quam.
+        Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in,
+        ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam.
+        Aenean laoreet. <b>Curabitur sit amet mauris</b>. Vestibulum nisi
+        lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus,
+        accumsan porttitor, cursus quis, aliquet eget, justo. Sed pretium
+        blandit orci.{" "}
+        <b>
+          Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit
+          amet, augue
+        </b>
+        . Ut eu diam at pede suscipit sodales.{" "}
+      </p>
+
+      <p>
+        Aenean lectus elit, fermentum non, convallis id, sagittis at, neque.
+        Nullam mauris orci, aliquet et, iaculis et, viverra vitae, ligula. Nulla
+        ut felis in purus aliquam imperdiet. Maecenas aliquet mollis lectus.
+        Vivamus consectetuer risus et tortor. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. <b>Ut eu diam at pede suscipit sodales</b>.
+        Integer nec odio. Praesent libero.{" "}
+        <b>
+          Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede
+        </b>
+        . Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh
+        elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec
+        tellus sed augue semper porta.{" "}
+      </p>
+
+      <p>
+        Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti
+        sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+        Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.
+        Curabitur tortor. <i>Integer nec odio</i>. Pellentesque nibh. Aenean
+        quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis
+        tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus
+        risus, iaculis vel, suscipit quis, luctus non, massa.{" "}
+      </p>
+
+      <p>
+        Fusce ac turpis quis ligula lacinia aliquet.{" "}
+        <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit</i>. Mauris
+        ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in,
+        nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu
+        ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec
+        ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing
+        diam, a cursus ipsum ante quis turpis.{" "}
+        <b>Fusce ac turpis quis ligula lacinia aliquet</b>. Nulla facilisi. Ut
+        fringilla.{" "}
+        <i>
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+          per inceptos himenaeos
+        </i>
+        . Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet.
+        Vestibulum sapien.{" "}
+      </p>
+
+      <p>
+        Proin quam. <i>Fusce ac turpis quis ligula lacinia aliquet</i>. Etiam
+        ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus.
+        Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra
+        auctor, sem massa mattis sem, at interdum magna augue eget diam.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed
+        non quam. <i>Nam nec ante</i>. In vel mi sit amet augue congue
+        elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus
+        nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt
+        malesuada tellus. Ut ultrices ultrices enim.{" "}
+      </p>
+
+      <p>
+        <b>
+          Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at
+          interdum magna augue eget diam
+        </b>
+        . Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper.
+        Nulla facilisi. <b>Praesent blandit dolor</b>. Integer lacinia
+        sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id
+        quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in,
+        ultrices sit amet, augue. <b>Nulla facilisi</b>. Proin sodales libero
+        eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo
+        ac, facilisis ac, ultricies eu, pede.{" "}
+      </p>
+
+      <p>
+        Ut orci risus, accumsan porttitor, cursus quis, aliquet eget, justo. Sed
+        pretium blandit orci. Ut eu diam at pede suscipit sodales. Aenean lectus
+        elit, fermentum non, convallis id, sagittis at, neque. Nullam mauris
+        orci, aliquet et, iaculis et, viverra vitae, ligula. Nulla ut felis in
+        purus aliquam imperdiet. <i>Nulla facilisi</i>. Maecenas aliquet mollis
+        lectus. Vivamus consectetuer risus et tortor. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Integer nec odio.{" "}
+        <i>Proin sodales libero eget ante</i>. Praesent libero.{" "}
+      </p>
+
+      <p>
+        Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum
+        imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed
+        augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+        inceptos himenaeos. <i>Nulla ut felis in purus aliquam imperdiet</i>.
+        Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.{" "}
+      </p>
+
+      <p>
+        Curabitur tortor. <i>Maecenas aliquet mollis lectus</i>. Pellentesque
+        nibh. Aenean quam. In scelerisque sem at dolor.{" "}
+        <b>Duis sagittis ipsum</b>. Maecenas mattis. Sed convallis tristique
+        sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus,
+        iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis
+        ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper
+        vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum
+        velit. Class aptent taciti sociosqu ad litora torquent per conubia
+        nostra, per inceptos himenaeos.{" "}
+      </p>
+
+      <p>
+        Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque
+        adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut
+        fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat
+        imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in
+        justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus
+        magna.{" "}
+      </p>
+
+      <p>
+        Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at
+        interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus
+        orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie
+        dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue
+        congue elementum. <i>Maecenas mattis</i>. Morbi in ipsum sit amet pede
+        facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas
+        et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices
+        enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar
+        ullamcorper. Nulla facilisi.{" "}
+      </p>
+
+      <p>
+        Integer lacinia sollicitudin massa. <b>Sed non quam</b>. Cras metus. Sed
+        aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis,
+        venenatis tristique, dignissim in, ultrices sit amet, augue. Proin
+        sodales libero eget ante. Nulla quam. Aenean laoreet.{" "}
+        <i>Integer euismod lacus luctus magna</i>. Vestibulum nisi lectus,
+        commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan
+        porttitor, cursus quis, aliquet eget, justo. Sed pretium blandit orci.
+        Ut eu diam at pede suscipit sodales.{" "}
+      </p>
+
+      <p>
+        Aenean lectus elit, fermentum non, convallis id, sagittis at, neque.
+        Nullam mauris orci, aliquet et, iaculis et, viverra vitae, ligula. Nulla
+        ut felis in purus aliquam imperdiet. Maecenas aliquet mollis lectus.
+        Vivamus consectetuer risus et tortor. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed
+        cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum
+        imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed
+        augue semper porta.{" "}
+      </p>
+
+      <p>
+        Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti
+        sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+        Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.{" "}
+        <i>Nulla ut felis in purus aliquam imperdiet</i>. Curabitur tortor.
+        Pellentesque nibh. Aenean quam. In scelerisque sem at dolor.{" "}
+        <b>Mauris massa</b>. Maecenas mattis. Sed convallis tristique sem. Proin
+        ut ligula vel nunc egestas porttitor.{" "}
+      </p>
+
+      <p>
+        <b>Mauris massa</b>. Morbi lectus risus, iaculis vel, suscipit quis,
+        luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris
+        ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in,
+        nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu
+        ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec
+        ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing
+        diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla.
+        Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet.{" "}
+      </p>
+
+      <p>
+        <b>Mauris ipsum</b>. Vestibulum sapien. Proin quam.{" "}
+        <i>Sed dignissim lacinia nunc</i>. Etiam ultrices. Suspendisse in justo
+        eu magna luctus suscipit. Sed lectus. <b>Suspendisse potenti</b>.
+        Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra
+        auctor, sem massa mattis sem, at interdum magna augue eget diam.{" "}
+        <b>Nam nec ante</b>. Vestibulum ante ipsum primis in faucibus orci
+        luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui.
+        Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue
+        elementum. Morbi in ipsum sit amet pede facilisis laoreet.{" "}
+        <b>Integer euismod lacus luctus magna</b>. Donec lacus nunc, viverra
+        nec, blandit vel, egestas et, augue.{" "}
+      </p>
+
+      <p>
+        Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim.
+        Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper.
+        Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed
+        aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis,
+        venenatis tristique, dignissim in, ultrices sit amet, augue. Proin
+        sodales libero eget ante. Nulla quam. Aenean laoreet.{" "}
+      </p>
+
+      <p>
+        Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut
+        orci risus, accumsan porttitor, cursus quis, aliquet eget, justo. Sed
+        pretium blandit orci. Ut eu diam at pede suscipit sodales. Aenean lectus
+        elit, fermentum non, convallis id, sagittis at, neque.{" "}
+        <b>Proin sodales libero eget ante</b>. Nullam mauris orci, aliquet et,
+        iaculis et, viverra vitae, ligula. Nulla ut felis in purus aliquam
+        imperdiet. Maecenas aliquet mollis lectus. Vivamus consectetuer risus et
+        tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        nec odio. Praesent libero. Sed cursus ante dapibus diam.{" "}
+      </p>
+
+      <p>
+        <i>Sed aliquet risus a tortor</i>. Sed nisi. Nulla quis sem at nibh
+        elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec
+        tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu
+        eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia
+        nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed
+        dignissim lacinia nunc. <b>Sed nisi</b>. Curabitur tortor.{" "}
+      </p>
+
+      <p>
+        Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas
+        mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas
+        porttitor. <b>Vestibulum lacinia arcu eget nulla</b>. Morbi lectus
+        risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis
+        quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus,
+        ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat
+        condimentum velit. Class aptent taciti sociosqu ad litora torquent per
+        conubia nostra, per inceptos himenaeos. Nam nec ante.{" "}
+        <i>Curabitur sodales ligula in libero</i>. Sed lacinia, urna non
+        tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis
+        turpis.{" "}
+      </p>
+
+      <p>
+        Nulla facilisi. Ut fringilla. Suspendisse potenti.{" "}
+        <b>Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa</b>
+        . Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin
+        quam. Etiam ultrices.{" "}
+        <b>
+          Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh
+        </b>
+        . Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer
+        euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor,
+        sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante
+        ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+        Morbi lacinia molestie dui. Praesent blandit dolor.{" "}
+      </p>
+    </SidebarWithHeader>
+  );
+}
